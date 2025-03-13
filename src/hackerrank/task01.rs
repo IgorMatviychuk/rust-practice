041b47f33889a4f756c818f5c1a904caf4e77777
Igor Matviychuk <111240677+IgorMatviychuk@users.noreply.github.com>
2025-03-13 10:41:37 +0200

    Update task01.rs

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,16 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    let mut numbers = String::new();
+    io::stdin().read_line(&mut numbers).unwrap();
+    
+    let sum: i32 = numbers
+        .split_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum();
+    
+    println!("{}", sum);
 }
