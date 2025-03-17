e50b00c643a9d827c6fafba43b4b97bbe9dac22d
Igor Matviychuk <111240677+IgorMatviychuk@users.noreply.github.com>
2025-03-17 14:03:22 +0200

    Update hw01.rs

--- src/homework/hw01.rs
+++ src/homework/hw01.rs
@@ -1,4 +1,19 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+fn main() {
+    const ENVELOPE_WIDTH: usize = 25;
+    const ENVELOPE_HEIGHT: usize = 10;
+
+    for y in 0..ENVELOPE_HEIGHT {
+        for x in 0..ENVELOPE_WIDTH {
+            let is_hor = y == 0 || y == ENVELOPE_HEIGHT - 1;
+            let is_ver = x == 0 || x == ENVELOPE_WIDTH - 1;
+            let is_diag1 = x == y * ENVELOPE_WIDTH / ENVELOPE_HEIGHT;
+            let is_diag2 = x == (ENVELOPE_HEIGHT - 1 - y) * ENVELOPE_WIDTH / ENVELOPE_HEIGHT;
+
+            let to_show = is_hor || is_ver || is_diag1 || is_diag2;
+
+            let sym = if to_show { '*' } else { ' ' };
+            print!("{}",sym);
+        }
+        println!();
+    }
 }
